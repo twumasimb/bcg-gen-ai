8ea9470e1e1962572444a8092f996d272cfccca9
Twumasi Mensah-Boateng <twumasimb@gmail.com>
2024-10-09 02:36:33 -0500

    add task two

--- task_two/chatbot.py
+++ task_two/chatbot.py
@@ -1,5 +1,15 @@
+import os
+import io
 import pandas as pd
 from typing import List, Dict
+
+import textwrap
+import pprint 
+import torch.nn.functional as F
+from transformers import logging
+from unsloth import FastLanguageModel
+from contextlib import redirect_stdout
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def simple_chatbot(user_query: str, df: pd.DataFrame) -> str:
     # Extract companies and years from the query
@@ -54,3 +64,32 @@ def get_data(df: pd.DataFrame, companies: List[str], years: List[int], param:str
                 results.append(f"{company} ({year}): {extracted_data[0]:,.2f}")
     
     return f"{param}:\n" + "\n".join(results) if results else "No data found for the specified companies and years."
+
+
+def llm_response(query:str, response:str):
+
+    # Create a template for the prompt
+    prompt = f"Given this query '{query}' and the response '{response}', write out the response in a way that is easy for non technical person to understand"
+
+    # Create a template for the prompt
+    return gemma_2B(prompt=prompt)
+
+def gemma_2B(prompt:str):
+    with redirect_stdout(io.StringIO()):
+        tokenizer = AutoTokenizer.from_pretrained("unsloth/gemma-2-2b-it-bnb-4bit")
+        model = AutoModelForCausalLM.from_pretrained("unsloth/gemma-2-2b-it-bnb-4bit")
+
+    inputs = tokenizer(prompt, return_tensors="pt")
+    outputs = model.generate(**inputs, max_length=2048, return_dict_in_generate=True)
+    
+    # Get the length of the input sequence
+    input_length = inputs['input_ids'].shape[1]
+    
+    # Decode the generated sequence, skipping the input tokens
+    response = tokenizer.decode(outputs.sequences[0][input_length:], skip_special_tokens=True)
+
+    return response
+
+def print_wrapped(text:str, wrap_length:int=80):
+    wrapped_text = textwrap.fill(text, wrap_length)
+    print(wrapped_text)
